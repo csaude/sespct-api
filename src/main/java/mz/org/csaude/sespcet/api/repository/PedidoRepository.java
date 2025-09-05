bc9c83533946552fc2bd933c95d6a288abb18dc1
Joao Nuno Mabjaia <94103183+joaonunomabjaia@users.noreply.github.com>
2025-09-05 12:21:37 +0200

    Comunicacao com Modulo

---
+++ src/main/java/mz/org/csaude/sespcet/api/repository/PedidoRepository.java
@@ -0,0 +1,26 @@
+package mz.org.csaude.sespcet.api.repository;
+
+import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.jpa.repository.JpaRepository;
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
+import mz.org.csaude.sespcet.api.entity.Pedido;
+
+import java.util.List;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    Page<Pedido> findByStatus(Pedido.Status status, Pageable pageable);
+
+    Pedido findByPedidoIdCtAndFacilityCode(String pedidoIdCt, String facilityCode);
+
+    // Buscar todos os pedidos correspondentes a uma lista de UUIDs
+    List<Pedido> findByUuidIn(List<String> uuids);
+
+    // Salvar/atualizar múltiplos pedidos de uma vez
+    @Override
+    <S extends Pedido> List<S> saveAll(Iterable<S> entities);
+}
+
+
